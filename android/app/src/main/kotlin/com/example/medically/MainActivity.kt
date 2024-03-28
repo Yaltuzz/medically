09b9323e51110d48342f2f90d66fe0c398d739c7
Jan Stopiński <84092150+Yaltuzz@users.noreply.github.com>
2024-03-28 17:34:15 +0100

    python execution (#3)

--- android/app/src/main/kotlin/com/example/medically/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/medically/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.medically
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+  }
 }
+
